c72c67250d1ee7926a988b60d910f66524448e6d
AccidentalRebel <accidentalrebel@gmail.com>
2022-07-07 03:14:23 +0400

    Call to ShellExecuteA now works.

--- av_evasion.asm
+++ av_evasion.asm
@@ -1,13 +1,29 @@
 	bits 64
 	default rel
 
+segment .data
+	msg_open	db	"open", 0
+	msg_notepad	db	"notepad", 0
+
 segment .text
 	global main
 	extern ExitProcess
+	extern ShellExecuteA
 
 main:
 	push    rbp
 	mov     rbp, rsp
+
+	push	0x5	
+	push	0x0
+	xor	r9, r9
+	lea	r8, [msg_notepad]	
+	lea	rdx, [msg_open]	
+	xor	rcx, rcx	
+
+	sub	rsp, 32
+	call	ShellExecuteA
+	add	rsp, 32
 
 	xor     rax, rax
 	call    ExitProcess
