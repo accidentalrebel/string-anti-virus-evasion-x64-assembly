dde5692832615da4fe37db2b714a85f4a63d43d6
AccidentalRebel <accidentalrebel@gmail.com>
2022-07-09 15:44:16 +0400

    Working version for part 2. Needs cleaning.

--- av-evasion.c
+++ av-evasion.c
@@ -4,5 +4,12 @@
 int main(void)
 {
 	char ca_notepad[] = { 'n','o','t','e','p','a','d',0 };
-	ShellExecute(0, "open", ca_notepad, NULL, NULL, SW_SHOW);
+	HINSTANCE hinst = ShellExecute(0, "open", ca_notepad, NULL, NULL, SW_SHOW);
+
+	char ca_notepad2[] = "notepad";
+	ShellExecute(0, "open", ca_notepad2, NULL, NULL, SW_SHOW);
+
+	/* HINSTANCE test = hinst; */
+	
+	ExitProcess(0);
 }
--- av_evasion.asm
+++ av_evasion.asm
@@ -3,11 +3,16 @@
 
 segment .data
 	msg_open	db	"open", 0
+	;; str_shell32	db	"shell32.dll", 0
+	;; str_shexa	db	"ShellExecuteA", 0
 
 segment .text
 	global main
 	extern ExitProcess
 	extern ShellExecuteA
+	extern LoadLibraryA
+	extern GetProcAddress
+	extern MessageBoxA
 
 %macro	addstr2stack	1-*
 	%assign	i 0
@@ -27,6 +32,51 @@ segment .text
 main:
 	push    rbp
 	mov     rbp, rsp
+ 	sub	rsp, 32
+
+	sub	rsp, 16
+	addstr2stack "s", "h", "e", "l", "l", "3", "2", ".", "d", "l", "l", 0x0
+	lea	rcx, [rsp]
+
+	;; lea	rcx, [str_shell32]
+	sub	rsp, 32
+	call	LoadLibraryA
+	add	rsp, 32
+
+	add	rsp, 16		; Release shell32.dll
+	
+	mov	rcx, rax
+
+	sub	rsp, 16
+	addstr2stack "S", "h", "e", "l", "l", "E", "x", "e", "c", "u", "t", "e", "A", 0x0
+	lea	rdx, [rsp]
+
+	;; lea	rdx, [str_shexa]
+ 	sub	rsp, 32
+	call	GetProcAddress
+	add	rsp, 32
+
+	add	rsp, 16  	; Release ShellExecuteA
+
+	mov	rbx, rax
+
+	;; push	rax
+	;; xor	r9, r9
+	;; lea	r8, [str_shexa]
+	;; lea	rdx, [str_shexa]
+	;; xor	rcx, rcx
+	;; sub	rsp, 32
+	;; call	rax
+	;; add	rsp, 32
+	;; pop	rax
+
+	;; xor	r9, r9
+	;; lea	r8, [str_shexa]
+	;; lea	rdx, [str_shexa]	
+	;; xor	rcx, rcx
+	;; sub	rsp, 32
+	;; call	rax
+	;; add	rsp, 32
 	
 	push	0x5
 	push	0x0
@@ -43,10 +93,10 @@ main:
 	;; add	rsp, 16
 	
 	lea	rdx, [msg_open]	
-	xor	rcx, rcx	
+	xor	rcx, rcx
 
 	sub	rsp, 32
-	call	ShellExecuteA
+	call	rbx
 	add	rsp, 32
 
 	xor     rax, rax
